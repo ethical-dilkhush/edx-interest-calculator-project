6e2082eb7f278c5297a310d27ebe62cfe2f179ba
Dilkhush Kumar <73588978+ethical-dilkhush@users.noreply.github.com>
2021-07-01 01:43:49 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,17 +2,36 @@
     function cal()
     {
       var amt=document.getElementById("amt").value;
-      var rate=document.getElementById("rate").value;
+      var rate=document.getElementById("rate").innerHTML;
       var year=document.getElementById("year").value;
-      var A= (amt*rate*year)/100;
-      var temp=parseInt(A);
-      var temp2=parseInt(amt);
-      var final=temp+temp2;
-      document.getElementById("d-amt").innerHTML=amt;
-      document.getElementById("d-rate").innerHTML=rate;
-      document.getElementById("d-time").innerHTML=year;
-      document.getElementById("d-final").innerHTML=final;
-      document.getElementById("output").style.display="block";
+      if(amt=="" || rate=="" || year=="" || amt<=0)
+      {
+      document.getElementById("errorpop").style.display="block";
+      document.getElementById("output").style.display="none";
+
+    }
+      else
+      {
+        document.getElementById("errorpop").style.display="none";
+
+            rate = rate.replace("%", "");
+            var A= (amt*rate*year)/100;
+            var temp=parseInt(A);
+            var temp2=parseInt(amt);
+            var final=temp+temp2;
+            
+            document.getElementById("d-amt").innerHTML=amt;
+            var drate=document.getElementById("rate").innerHTML;
+            document.getElementById("d-rate").innerHTML=drate;
+            
+            document.getElementById("d-final").innerHTML=final;
+            document.getElementById("output").style.display="block";
+            year=parseInt(year);
+            var dt = new Date();
+            var yr=dt.getYear();
+            yr=yr+1900+year;
+            document.getElementById("d-time").innerHTML=yr;
+      }
     }
  
  
@@ -22,4 +41,10 @@
         event.preventDefault();
       }
     }
-  
+    function showvalues(val) {
+      document.getElementById('rate').innerHTML=val+"%"; 
+     
+    }
+    function goBack(){
+      window.history.back();
+    }
